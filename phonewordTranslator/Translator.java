3be4d2a1a3a1dd1a34b0ce13b34f110c34d345c7
ChaosMillenium <darkrai.ivan@gmail.com>
2019-01-03 01:48:04 +0100

    Add files via upload

---
+++ phonewordTranslator/Translator.java
@@ -0,0 +1,106 @@
+package phonewordTranslator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Translator {
+
+	private Map<Character, String> textToPhoneMap;
+	private Map<String, Character> phoneToTextMap;
+
+	public Translator() {
+		textToPhoneMap = new HashMap<>(55);
+		textToPhoneMap.put('A', "2");
+		textToPhoneMap.put('B', "22");
+		textToPhoneMap.put('C', "222");
+		textToPhoneMap.put('D', "3");
+		textToPhoneMap.put('E', "33");
+		textToPhoneMap.put('F', "333");
+		textToPhoneMap.put('G', "4");
+		textToPhoneMap.put('H', "44");
+		textToPhoneMap.put('I', "444");
+		textToPhoneMap.put('J', "5");
+		textToPhoneMap.put('K', "55");
+		textToPhoneMap.put('L', "555");
+		textToPhoneMap.put('M', "6");
+		textToPhoneMap.put('N', "66");
+		textToPhoneMap.put('O', "666");
+		textToPhoneMap.put('P', "7");
+		textToPhoneMap.put('Q', "77");
+		textToPhoneMap.put('R', "777");
+		textToPhoneMap.put('S', "7777");
+		textToPhoneMap.put('T', "8");
+		textToPhoneMap.put('U', "88");
+		textToPhoneMap.put('V', "888");
+		textToPhoneMap.put('W', "9");
+		textToPhoneMap.put('X', "99");
+		textToPhoneMap.put('Y', "999");
+		textToPhoneMap.put('Z', "9999");
+		textToPhoneMap.put(' ', "0");
+		inverseMap();
+		textToPhoneMap.put('a', "2");
+		textToPhoneMap.put('b', "22");
+		textToPhoneMap.put('c', "222");
+		textToPhoneMap.put('d', "3");
+		textToPhoneMap.put('e', "33");
+		textToPhoneMap.put('f', "333");
+		textToPhoneMap.put('g', "4");
+		textToPhoneMap.put('h', "44");
+		textToPhoneMap.put('i', "444");
+		textToPhoneMap.put('j', "5");
+		textToPhoneMap.put('k', "55");
+		textToPhoneMap.put('l', "555");
+		textToPhoneMap.put('m', "6");
+		textToPhoneMap.put('n', "66");
+		textToPhoneMap.put('o', "666");
+		textToPhoneMap.put('p', "7");
+		textToPhoneMap.put('q', "77");
+		textToPhoneMap.put('r', "777");
+		textToPhoneMap.put('s', "7777");
+		textToPhoneMap.put('t', "8");
+		textToPhoneMap.put('u', "88");
+		textToPhoneMap.put('v', "888");
+		textToPhoneMap.put('w', "9");
+		textToPhoneMap.put('x', "99");
+		textToPhoneMap.put('y', "999");
+		textToPhoneMap.put('z', "9999");
+	}
+
+	private void inverseMap() {
+		phoneToTextMap = textToPhoneMap.entrySet().stream()
+				.collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+	}
+
+	public String translateTextToPhone(String toTranslate) {
+		StringBuilder sb = new StringBuilder();
+		for (Character ch : toTranslate.toCharArray()) {
+			if (textToPhoneMap.containsKey(ch)) {
+				sb.append(textToPhoneMap.get(ch));
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
+	}
+
+	public String translatePhoneToText(String toTranslate) {
+		StringBuilder tt = new StringBuilder(toTranslate);
+		tt.append(' ');
+		StringBuilder sb = new StringBuilder();
+		StringBuilder phoneword = new StringBuilder();
+		boolean lastIsBlank = false;
+		for (Character ch : tt.toString().toCharArray()) {
+			if (ch.equals(' ') && !lastIsBlank) {
+				if (phoneToTextMap.containsKey(phoneword.toString())) {
+					sb.append(phoneToTextMap.get(phoneword.toString()));
+				}
+				phoneword.setLength(0);
+				lastIsBlank = true;
+			} else {
+				phoneword.append(ch);
+				lastIsBlank = false;
+			}
+		}
+		return sb.toString();
+	}
+}
---
+++ phonewordTranslator/TranslatorView.java
@@ -0,0 +1,130 @@
+package phonewordTranslator;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class TranslatorView {
+
+	private JFrame frmPhonewordTranslator;
+	private Translator translator;
+	private JTextArea textArea, textArea_1;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TranslatorView window = new TranslatorView();
+					window.frmPhonewordTranslator.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TranslatorView() {
+		translator = new Translator();
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPhonewordTranslator = new JFrame();
+		frmPhonewordTranslator.setTitle("Phoneword Translator");
+		frmPhonewordTranslator.setBounds(100, 100, 450, 300);
+		frmPhonewordTranslator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frmPhonewordTranslator.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel lblText = new JLabel("Text");
+		GridBagConstraints gbc_lblText = new GridBagConstraints();
+		gbc_lblText.insets = new Insets(0, 0, 5, 5);
+		gbc_lblText.gridx = 0;
+		gbc_lblText.gridy = 0;
+		frmPhonewordTranslator.getContentPane().add(lblText, gbc_lblText);
+		
+		JLabel lblPhoneword = new JLabel("Phoneword");
+		GridBagConstraints gbc_lblPhoneword = new GridBagConstraints();
+		gbc_lblPhoneword.insets = new Insets(0, 0, 5, 0);
+		gbc_lblPhoneword.gridx = 1;
+		gbc_lblPhoneword.gridy = 0;
+		frmPhonewordTranslator.getContentPane().add(lblPhoneword, gbc_lblPhoneword);
+		
+		textArea = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridheight = 7;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 1;
+		frmPhonewordTranslator.getContentPane().add(scrollPane, gbc_scrollPane);
+		
+		textArea_1 = new JTextArea();
+		JScrollPane scrollPane_1 = new JScrollPane(textArea_1);
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.gridheight = 7;
+		gbc_scrollPane_1.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 1;
+		gbc_scrollPane_1.gridy = 1;
+		frmPhonewordTranslator.getContentPane().add(scrollPane_1, gbc_scrollPane_1);
+		
+		JButton btnTextToPhoneword = new JButton("Text to Phoneword");
+		btnTextToPhoneword.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				textArea_1.setText(translator.translateTextToPhone(textArea.getText()));
+			}
+		});
+		GridBagConstraints gbc_btnTextToPhoneword = new GridBagConstraints();
+		gbc_btnTextToPhoneword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnTextToPhoneword.insets = new Insets(0, 0, 0, 5);
+		gbc_btnTextToPhoneword.gridx = 0;
+		gbc_btnTextToPhoneword.gridy = 8;
+		frmPhonewordTranslator.getContentPane().add(btnTextToPhoneword, gbc_btnTextToPhoneword);
+		
+		JButton btnPhonewordToText = new JButton("Phoneword to Text");
+		btnPhonewordToText.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				textArea.setText(translator.translatePhoneToText(textArea_1.getText()));
+			}
+		});
+		GridBagConstraints gbc_btnPhonewordToText = new GridBagConstraints();
+		gbc_btnPhonewordToText.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnPhonewordToText.gridx = 1;
+		gbc_btnPhonewordToText.gridy = 8;
+		frmPhonewordTranslator.getContentPane().add(btnPhonewordToText, gbc_btnPhonewordToText);
+	}
+
+}
